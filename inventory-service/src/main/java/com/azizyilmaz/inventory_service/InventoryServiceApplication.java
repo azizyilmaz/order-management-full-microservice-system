0246355ac67203b235186db48226040a3e1f447b
azizyilmaz <azizxyilmaz@outlook.com>
2026-03-19 23:28:01 +0300

    Inventory service is ready to use.

---
+++ inventory-service/src/main/java/com/azizyilmaz/inventory_service/InventoryServiceApplication.java
@@ -0,0 +1,32 @@
+package com.azizyilmaz.inventory_service;
+
+import com.azizyilmaz.inventory_service.model.Inventory;
+import com.azizyilmaz.inventory_service.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+        return args -> {
+            Inventory inventory = new Inventory();
+            inventory.setSkuCode("SKU-001");
+            inventory.setQuantity(100);
+            inventoryRepository.save(inventory);
+
+            Inventory inventory2 = new Inventory();
+            inventory2.setSkuCode("SKU-002");
+            inventory2.setQuantity(0);
+            inventoryRepository.save(inventory2);
+        };
+    }
+
+}
