329e8cec63dd828b0c5912d6f310aaef537ac2b2
azizyilmaz <azizxyilmaz@outlook.com>
2026-03-28 23:36:36 +0300

    Call inventory-service, and place order if product is in stock.

--- inventory-service/src/main/java/com/azizyilmaz/inventory_service/controller/InventoryController.java
+++ inventory-service/src/main/java/com/azizyilmaz/inventory_service/controller/InventoryController.java
@@ -1,20 +1,23 @@
 package com.azizyilmaz.inventory_service.controller;
 
+import com.azizyilmaz.inventory_service.dto.InventoryResponse;
 import com.azizyilmaz.inventory_service.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
 @RequiredArgsConstructor
 public class InventoryController {
 
-    private InventoryService inventoryService;
+    private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         return inventoryService.isInStock(skuCode);
     }
 }
--- inventory-service/src/main/java/com/azizyilmaz/inventory_service/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/azizyilmaz/inventory_service/repository/InventoryRepository.java
@@ -3,8 +3,9 @@ package com.azizyilmaz.inventory_service.repository;
 import com.azizyilmaz.inventory_service.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    Optional<Inventory> findBySkuCode();
+
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
